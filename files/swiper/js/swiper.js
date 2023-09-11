e004a610d73446907d2564e4f26ab4c1ef5f8520
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2023-09-11 15:16:23 +0300

    Add files via upload

--- files/swiper/js/swiper.js
+++ files/swiper/js/swiper.js
@@ -262,3 +262,104 @@ new Swiper('.you-get-slider', {
       }
    },
 });
+
+
+var scrollEvents = ['wheel', 'mousewheel']
+function freezeScroll() {
+   for (var i = 0; i < arguments.length; i++) {
+      elem = arguments[i];
+      let func = preventScrollEventFunc(elem);
+      let options = {passive: false};
+      $(elem).on('mouseenter', function() {
+         onWheel(window, func, options);
+      }).on('mouseleave', function() {
+         removeOnWheel(window, func);
+      });
+   }
+}
+// Отменить скролл страницы, если элемент selector прокручен до упора
+function preventScrollEventFunc(selector) {
+   let elem = $(selector);
+   function preventScroll(e) {
+      let offset = e.wheel || e.wheelDelta;
+      let crossingUpper = elem.scrollTop() == 0 && offset > 0;
+      let crossingDown = (elem[0].scrollHeight - elem.scrollTop() ==
+         elem[0].clientHeight && offset < 0);
+      if (crossingUpper || crossingDown) {
+         e.preventDefault()
+      }
+   }
+   return preventScroll;
+}
+// Повесить обработчик func для событий wheel и mousewheel у elem
+function onWheel(elem, func, options) {
+   options = options || {};
+   scrollEvents.forEach(function(item, i, arr) {
+      elem.addEventListener(item, func, options);
+   });
+}
+// Убрать обработчик func для событий wheel и mousewheel у elem
+function removeOnWheel(elem, func, options) {
+   options = options || {};
+   scrollEvents.forEach(function(item, i, arr) {
+      elem.removeEventListener(item, func, options);
+   });
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+   //freezeScroll('#div', '#div2', #div3', '#div...');
+   freezeScroll('#showroom-area-slider');
+
+});
+
+const showroom_area_slider = document.querySelector('.showroom-area-slider')
+new Swiper(showroom_area_slider, {
+   slidesPerView: 2.03,
+   slidesPerGroup: 1,
+   watchOverflow: true,
+   autoHeight: false,
+   centeredSlides: true,
+   loop: true,
+   spaceBetween: 25,
+   //initialSlide: 1,
+   speed: 1200,
+   effect: 'slide',
+   mousewheel: {
+      sensitivity: 3,
+      eventsTarget: showroom_area_slider,
+   },
+   breakpoints: {
+      0: {
+         slidesPerView: 1.14,
+         slidesPerGroup: 1,
+         spaceBetween: 20,
+      },
+      576: {
+         slidesPerView: 1.6,
+         slidesPerGroup: 1,
+         spaceBetween: 25,
+      },
+      800: {
+         slidesPerView: 2.03,
+         slidesPerGroup: 1,
+         spaceBetween: 25,
+      }
+   }
+
+})
+//document.querySelector('.showroom-area-slider').addEventListener("wheel", (event) => {});
+
+/*$('#js-showroom-area').bind('mousewheel DOMMouseScroll', function(e) {
+   var scrollTo = null;
+   if (e.type == 'mousewheel') {
+      scrollTo = (e.originalEvent.wheelDelta * -1);
+   } else if (e.type == 'DOMMouseScroll') {
+      scrollTo = 40 * e.originalEvent.detail;
+   }
+   if (scrollTo) {
+      e.preventDefault();
+      $(this).scrollTop(scrollTo + $(this).scrollTop());
+   }
+});*/
